c0aaf5be35696182a4b2708c54eb2ba098e275ca
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-02-12 15:25:56 -0200

    add artist flag

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ NAME:
   musicli - It does something related to music
 
 USAGE:
-  musicli bla bla bla
+  musicli [--options]
 
 VERSION:
   %s
@@ -21,7 +21,7 @@ COMMANDS:
   help, h Shows a list of commands or help
 
 EXAMPLES:
-  musicli bla bla bla 
+  musicli -artist="Paul Mccartney"
 
 OPTIONS:
 `, Version)
@@ -30,15 +30,28 @@ OPTIONS:
 
 var Version = "0.0.1"
 
+func Run() {
+	fmt.Println("Runnig...")
+}
+
 func main() {
 
 	version := flag.Bool("version", false, "display the version number")
+	artist := flag.String("artist", "", "artist's name for searching")
+
 	flag.Usage = Usage
 	flag.Parse()
 
 	if *version {
 		fmt.Printf("version %s\n", Version)
 		os.Exit(1)
+	}
+
+	if *artist != "" {
+		fmt.Fprintf(os.Stdout, "You have selected the artist: %s\n", *artist)
+		Run()
+	} else {
+		fmt.Println("You should enter an artist name in order to proceed.")
 	}
 
 }
